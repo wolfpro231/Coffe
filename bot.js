4e3160a4c67deff3ba154a30ebe991ab56387a40
MostafaProgrammers <52133708+MostafaProgrammers@users.noreply.github.com>
2019-07-02 13:41:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,42 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "-";
 
+
+const developers = ["567694723679125514","id"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+     
+  if (message.content.startsWith(prefix + 'pl')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (prefix + "hajcjss")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(prefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`** ${argresult}  تم تغيير بلاينق الي**`)
+  } else
+  if (message.content.startsWith(prefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**${argresult} تم تغيير ليسنينق الي**`)
+  } else
+  if (message.content.startsWith(prefix + 'tw')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/ii7m0dy");
+      message.channel.send(`**تم تغيير التويتش **`)
+  }
+  if (message.content.startsWith(prefix + 'setna')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**${argresult} تم تغيير الاسم الي** `)
+} else
+if (message.content.startsWith(prefix + 'setav')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**${argresult} تم تغيير الصورة الي** `);
+}
+});
+
+
 client.on('message', message => {
 if(message.author.bot) return;
 if(message.channel.type === 'dm') return;
